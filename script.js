e62caf1496ee7b0b41032e63072a2c5a17c60e2a
SchimttJhonatan12 <jhonatan.schmitt@escola.pr.gov.br>
2025-09-16 16:57:41 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -7,3 +7,44 @@ botaomostrapalavras.addEventListener("clik", mostrapalavraschave);
 
         camporesultado. textContent = palavras.join(",");
     }
+   function processatexto (texto) {
+   let palavras = texto.split(/\P{L}+U);
+   const frequencias = contafrequecias(palavras);
+   let ordenadas = object.keys(frequencias).sort(ordenaPalavra);
+
+   function ordenaPalavras(p1,p2){
+    return frequencias [p2]- frequencias[p1];
+   }
+    console.log(ordenadas);
+    return ordenadas.slice(0,10);
+
+
+
+
+
+
+]
+ 
+ fuction contrafrequencias(palavras){
+    let frequencias ={};
+    for (let i of of palavras){
+        frequencias{i} = 0;
+        for(let j of palavras){
+            if (i==j){
+                frequencias{i}++;
+            }
+        }
+    }
+ }
+
+
+
+
+
+
+
+
+   return palavras;
+
+   }
+    
